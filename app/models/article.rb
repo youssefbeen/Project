23706933bf2be981e6d9e21f566fac5e0b157dc0
Youssef <mister.ucef@gmail.com>
2018-04-21 17:17:26 +0100

    validate model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    validates :title, :description, :price, presence: true
+    validates :description, length: {minimum: 30}
+    validates :price, numericality: {only_integer: true}
 end
