06eadd7f14fb80ce9c89afdc31fe09b087fe9f32
Youssef <mister.ucef@gmail.com>
2018-05-13 14:50:50 +0000

    heroku prob

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module Project
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-      
-    config.serve_static_assets = true
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
